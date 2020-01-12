cb53466972053fc37b80dd3fd0c2cd03c145ad4d
Alex <spirit@spiritpact.pw>
2020-01-11 19:29:24 -0700

    begin arenas

--- src/main/java/com/conaxgames/practice/arena/Arena.java
+++ src/main/java/com/conaxgames/practice/arena/Arena.java
@@ -1,4 +1,27 @@
 package com.conaxgames.practice.arena;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import org.bukkit.Location;
+
+@Data
 public class Arena {
+
+    /**
+     * The internal ID of this arena. This field
+     * is used to load, save, and identify specific
+     * arenas.
+     */
+    @SerializedName("_id") private final String name;
+
+    /**
+     * Locations players spawn at when a match starts.
+     */
+    private final Location spawnA, spawnB;
+
+    /**
+     * Whether or not this arena is in use.
+     */
+    private boolean beingUsed;
+
 }
